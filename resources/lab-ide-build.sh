fe64ccb0ffa0cdb4086cba03213217f6e7f5effb
Jason Forte <fortejas@amazon.com>
2018-05-14 10:24:02 +0200

    Updated the cloudformation template to open ports 80 and 443 instead of in the startup script.

--- resources/lab-ide-build.sh
+++ resources/lab-ide-build.sh
@@ -6,28 +6,6 @@ sudo yum -y install jq
 # Install bash-completion
 sudo yum install bash-completion -y
 
-aws ec2 describe-security-groups \
-    --group-name $(curl -s http://169.254.169.254/latest/meta-data/security-groups/ | head -n 1) | \
-    jq '.SecurityGroups[0].IpPermissions[].ToPort' | grep 80 && echo "Port 80 Open"
-
-if [ $? -eq 1 ]; then
-  # Allow access on port 80
-  aws ec2 authorize-security-group-ingress \
-      --group-name $(curl -s http://169.254.169.254/latest/meta-data/security-groups/ | head -n 1) \
-      --protocol tcp --port 80 --cidr 0.0.0.0/0
-fi
-
-aws ec2 describe-security-groups \
-    --group-name $(curl -s http://169.254.169.254/latest/meta-data/security-groups/ | head -n 1) | \
-    jq '.SecurityGroups[0].IpPermissions[].ToPort' | grep 443 && echo "Port 443 Open"
-
-if [ $? -eq 1 ]; then
-  # Allow access on port 443
-  aws ec2 authorize-security-group-ingress \
-      --group-name $(curl -s http://169.254.169.254/latest/meta-data/security-groups/ | head -n 1) \
-      --protocol tcp --port 443 --cidr 0.0.0.0/0
-fi
-
 if [ ! -d "docker-workshop/" ]; then
   # Download lab Repository
   git clone https://github.com/marcossantiago/docker-workshop.git
